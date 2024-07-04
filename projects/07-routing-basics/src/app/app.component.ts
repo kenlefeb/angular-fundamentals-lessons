685a7b17ad4bd9a90f99bb15e6bbd89f81bf19c9
Ken LeFebvre <kenneth@lefebvre.us>
2024-07-04 19:31:51 -0400

    chore: Enable routing in app component and add default path to greetings component

--- projects/07-routing-basics/src/app/app.component.ts
+++ projects/07-routing-basics/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule],
-  template: ` <h1>Enable routing to see the greeting below</h1> `,
-  styles: [],
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterModule],
+    template: ` <h1>Enable routing to see the greeting below</h1>
+        <router-outlet />`,
+    styles: [],
 })
 export class AppComponent {
-  title = '07-routing-basics';
+    title = '07-routing-basics';
 }
