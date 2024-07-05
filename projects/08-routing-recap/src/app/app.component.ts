9cac69ec7fab6f7240df1ba68636ca66f6242101
Ken LeFebvre <kenneth@lefebvre.us>
2024-07-04 20:22:10 -0400

    chore: Update app component to include navigation and product details section

--- projects/08-routing-recap/src/app/app.component.ts
+++ projects/08-routing-recap/src/app/app.component.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: ` <h1>Welcome to {{ title }}!</h1> `,
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, RouterLink],
+    template: ` <h1>Welcome to {{ title }}!</h1>
+        <section>
+            <h2>Navigation</h2>
+            <ul>
+                <li><a routerLink="/">Home</a></li>
+                <li><a routerLink="/products">Products</a></li>
+            </ul>
+        </section>
+        <section>
+            <h2>Products</h2>
+            <ul>
+                @for (title of productTitles; track $index) {
+                <li>
+                    <a [routerLink]="['details', $index]"
+                        >{{ $index }}. {{ title }}</a
+                    >
+                </li>
+                }
+            </ul>
+        </section>
+        <section>
+            <router-outlet />
+        </section>`,
+    styles: [
+        `
+            section {
+                margin-top: 10px;
+                margin-bottom: 20px;
+            }
+        `,
+    ],
 })
 export class AppComponent {
-  title = '08-routing-recap';
+    title = '08-routing-recap';
 
-  productTitles = ['Product 1', 'Product 2', 'Product 3'];
+    productTitles = ['Product 1', 'Product 2', 'Product 3'];
 }
--- projects/08-routing-recap/src/app/details/details.component.ts
+++ projects/08-routing-recap/src/app/details/details.component.ts
@@ -1,37 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-details',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    <section>
-      <p>Title</p>
-      <ul>
-        <li>Price</li>
-        <li>Description</li>
-      </ul>
-    </section>
-  `,
-  styles: ``,
+    selector: 'app-details',
+    standalone: true,
+    imports: [CommonModule],
+    template: `
+        <section>
+            <p>{{ this.productList[this.productId].title }}</p>
+            <ul>
+                <li>
+                    Price: &dollar;{{ this.productList[this.productId].price }}
+                </li>
+                <li>
+                    Description:
+                    {{ this.productList[this.productId].description }}
+                </li>
+            </ul>
+        </section>
+    `,
+    styles: ``,
 })
 export class DetailsComponent {
-  productList = [
-    {
-      title: 'Product 1',
-      price: 9234,
-      description: 'Product 1 is the best',
-    },
-    {
-      title: 'Product 2',
-      price: 3043,
-      description: 'Product 2 is special',
-    },
-    {
-      title: 'Product 3',
-      price: 4355,
-      description: 'Product 3 has my heart',
-    },
-  ];
+    productId = -1;
+
+    @Input()
+    set id(value: number) {
+        this.productId = value;
+        console.log('Product ID:', this.productId);
+    }
+
+    productList = [
+        {
+            title: 'Product 1',
+            price: 9234,
+            description: 'Product 1 is the best',
+        },
+        {
+            title: 'Product 2',
+            price: 3043,
+            description: 'Product 2 is special',
+        },
+        {
+            title: 'Product 3',
+            price: 4355,
+            description: 'Product 3 has my heart',
+        },
+    ];
 }
