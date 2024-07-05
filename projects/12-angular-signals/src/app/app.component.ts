7d9bcfb6a3b20ac74749bb7d4e50a82f654e325a
Ken LeFebvre <ken.lefebvre@clev.frb.org>
2024-07-05 11:43:36 -0400

    Got the 12-angular-signals exercise working, but without signals! - Got NG8: Type 'WritableSignal<Todo[]>' must have a '[Symbol.iterator]()' method that returns an iterator.

--- projects/12-angular-signals/src/app/app.component.ts
+++ projects/12-angular-signals/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Todo } from './todo';
 
@@ -6,9 +6,38 @@ import { Todo } from './todo';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
-  template: ` <h1>Building a TODO List</h1> `,
+  template: `
+    <h1>Building a TODO List</h1>
+    <section>
+        @for (todo of todos; track todo.id) {
+            <label [ngStyle]="{'text-decoration': todo.completed ? 'line-through' : 'none'}">
+                <input type="checkbox" [checked]="todo.completed" (change)="updateTodo(todo)" title="todo.title" /> {{ todo.title }}
+        </label>
+        }
+    </section>
+  `,
   styles: `label { display: block }`,
 })
 export class AppComponent {
-  updateTodo(todo: Todo) {}
+  updateTodo(todo: Todo) {
+    todo.completed = !todo.completed;
+    console.log(`${todo.id}. ${todo.title}: ${todo.completed}`);
+  }
+  todos: Todo[] = [
+    {
+      id: 1,
+      title: "Learn Angular",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Learn TypeScript",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Learn RxJS",
+      completed: false,
+    },
+  ];
 }
