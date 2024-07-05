3ccce068710ab6caace86f59084df99d880b0d46
Ken LeFebvre <kenneth@lefebvre.us>
2024-07-04 20:45:04 -0400

    chore: Update app component to include input fields for post title and body

--- projects/09-template-driven-forms/src/app/app.component.ts
+++ projects/09-template-driven-forms/src/app/app.component.ts
@@ -1,25 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  template: `
-    <article>
-      <h1>Blog Post</h1>
-      <section>
-        <label for="title">Post Title</label>
-        <!-- add the input -->
+    selector: 'app-root',
+    standalone: true,
+    imports: [FormsModule],
+    template: `
+        <article>
+            <h1>Blog Post</h1>
+            <section>
+                <label
+                    >Post Title
+                    <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        [(ngModel)]="title"
+                /></label>
 
-        <label for="body">Post Body</label>
-        <!-- add the textarea -->
-      </section>
-      <!-- <section>
-        <p>Display title</p>
-        <p>Display value</p>
-      </section> -->
-    </article>
-  `,
+                <label
+                    >Post Body
+                    <textarea
+                        id="body"
+                        name="body"
+                        [(ngModel)]="body"
+                    ></textarea>
+                </label>
+            </section>
+            <section>
+                <p>Title: {{ title }}</p>
+                <p>Body: {{ body }}</p>
+            </section>
+        </article>
+    `,
 })
 export class AppComponent {
-  title = '09-template-driven-forms';
+    title: string = '';
+    body: string = '';
 }
