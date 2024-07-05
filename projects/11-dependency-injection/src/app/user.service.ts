792bb0604a8638790485a93fa21920391aca38c0
Ken LeFebvre <kenneth@lefebvre.us>
2024-07-04 21:24:09 -0400

    chore: Update app component to include lazy loading for user info components

--- projects/11-dependency-injection/src/app/user.service.ts
+++ projects/11-dependency-injection/src/app/user.service.ts
@@ -1,13 +1,15 @@
+import { Injectable } from '@angular/core';
 import { data, User } from './data';
 
+@Injectable({ providedIn: 'root' })
 export class UserService {
-  private userData: User[] = data;
+    private userData: User[] = data;
 
-  constructor() {}
+    constructor() {}
 
-  getUserData(): Promise<User[]> {
-    return new Promise((resolve) => {
-      resolve(this.userData);
-    });
-  }
+    getUserData(): Promise<User[]> {
+        return new Promise((resolve) => {
+            resolve(this.userData);
+        });
+    }
 }
